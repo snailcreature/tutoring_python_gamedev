fcb6242fc7727cdf42b442c3c1d86ce819b2d26e
snailcreature <samuel.drage@myport.ac.uk>
2024-03-19 17:00:11 +0000

    Added circle and movement

--- main.py
+++ main.py
@@ -3,11 +3,20 @@ import pygame
 pygame.init()
 
 # Set the screen size
-screen = pygame.display.set_mode([500, 500])
+screen = pygame.display.set_mode([600, 500])
+
+clock = pygame.time.Clock()
+
+x = 250
+y = 250
+
+dx = 100
+dy = 100
 
 # Set up the main loop
 running = True
 while running:
+    dt = clock.tick()
 
     # End the main loop if the window is closed
     for event in pygame.event.get():
@@ -18,6 +27,15 @@ while running:
     screen.fill((225, 225, 225))
 
     # New code here
+    pygame.draw.circle(screen, (0, 0, 255), (x, y), 50)
+
+    if y <= 50 or y >= 450:
+        dy *= -1
+    if x <= 50 or x >= 550:
+        dx *= -1
+
+    x += dx * (dt/1000)
+    y += dy * (dt/1000)
 
     # Update the window
     pygame.display.update()
